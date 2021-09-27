3c5ac34b199b9ff72597d46a5c56d9b7d7285a56
Attila Lendvai <attila@lendvai.name>
2021-09-27 13:47:11 +0200

    tests: Smarten up git repository testing framework.
    * guix/tests/git.scm (with-git-repository): New macro that can be used in
    a nested way under a with-temporary-git-repository.
    (populate-git-repository): Extend the DSL with (add "some-noise"), (reset
    "[commit hash]"), (checkout "branch" orphan).
    * guix/tests/gnupg.scm (key-fingerprint-vector): New function.

--- guix/tests/gnupg.scm
+++ guix/tests/gnupg.scm
@@ -33,6 +33,7 @@
 
             read-openpgp-packet
             key-fingerprint
+            key-fingerprint-vector
             key-id))
 
 (define gpg-command
@@ -76,7 +77,10 @@ process is terminated afterwards."
    (open-bytevector-input-port
     (call-with-input-file file read-radix-64))))
 
+(define key-fingerprint-vector
+  (compose openpgp-public-key-fingerprint
+           read-openpgp-packet))
+
 (define key-fingerprint
   (compose openpgp-format-fingerprint
-           openpgp-public-key-fingerprint
-           read-openpgp-packet))
+           key-fingerprint-vector))
